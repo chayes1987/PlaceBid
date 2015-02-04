99f3c86f7a98d7e94280d4e4cb279363f15a2c7a
Conor Hayes <ch1987@live.ie>
2015-02-04 17:49:01 +0000

    Added method header for place bid. Annotated method as web method

--- src/PlaceBid.java
+++ src/PlaceBid.java
@@ -7,4 +7,10 @@ public class PlaceBid extends Application {
     public static void main(String[] args) {
 
     }
+
+    @GET
+    @Path("placebid/{id}/{email}")
+    public boolean placeBid(@PathParam("id") String id, @PathParam("email") String email){
+        return false;
+    }
 }
