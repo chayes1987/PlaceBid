5f21a427cd5818271aa92bb8ab6af99899208ba3
Conor Hayes <ch1987@live.ie>
2015-02-04 17:57:36 +0000

    Added constants for the 0mq addresses. Bound the publisher to the address

--- src/PlaceBid.java
+++ src/PlaceBid.java
@@ -8,14 +8,19 @@ import javax.ws.rs.core.Application;
 public class PlaceBid extends Application {
     private Context context = ZMQ.context();
     private Socket publisher = context.socket(ZMQ.PUB);
+    private final String PUB_ADR = "tcp://127.0.0.1:1101", ACK_ADR = "tcp://127.0.0.1:1110";
 
-    public static void main(String[] args) {
+    public static void main(String[] args) { new PlaceBid().subscribe(); }
 
+    private void subscribe(){
+        publisher.bind(PUB_ADR);
+        // TODO Implement subscriber
     }
 
     @GET
     @Path("placebid/{id}/{email}")
     public boolean placeBid(@PathParam("id") String id, @PathParam("email") String email){
+        // TODO Implement method
         return false;
     }
 }
