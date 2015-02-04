8c00d3fe33edd7674e9bc1e29988ea29b6b42cf0
Conor Hayes <ch1987@live.ie>
2015-02-04 17:51:46 +0000

    Added JAR for 0mq integration, added imports to class and initialized context and publisher objects

--- src/PlaceBid.java
+++ src/PlaceBid.java
@@ -1,9 +1,14 @@
+import org.jeromq.ZMQ;
+import org.jeromq.ZMQ.*;
 import javax.ws.rs.*;
 import javax.ws.rs.core.Application;
 
 @ApplicationPath("/bidder")
 @Path("/services")
 public class PlaceBid extends Application {
+    private Context context = ZMQ.context();
+    private Socket publisher = context.socket(ZMQ.PUB);
+
     public static void main(String[] args) {
 
     }
