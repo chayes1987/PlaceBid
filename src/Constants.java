bb8c30bf84761b32e2e7df25aad77d36051581a6
Conor Hayes <ch1987@live.ie>
2015-02-21 21:54:27 +0000

    Finalized addressing for AWS

--- src/Constants.java
+++ src/Constants.java
@@ -2,7 +2,7 @@
 public class Constants {
     public static final String APPLICATION_PATH = "/bidder";
     public static final String PATH = "/services";
-    public static final String PUB_ADR = "tcp://*:1101";
-    public static final String ACK_ADR = "tcp://172.31.32.23:1110";
+    public static final String PUB_ADR = "tcp://*:2900";
+    public static final String ACK_ADR = "tcp://172.31.32.23:3000";
     public static final String BID_PLACED_ACK_TOPIC = "ACK: BidPlaced";
 }
