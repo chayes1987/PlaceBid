4574d9c2a744d6acad6bfe8c7e402cac9c870250
Conor Hayes <ch1987@live.ie>
2015-04-26 18:00:03 +0100

    Added comments and coding standards

--- src/Constants.java
+++ src/Constants.java
@@ -1,4 +1,14 @@
+/**
+ * @author Conor Hayes
+ */
 
+/*
+    Coding Standards -> http://www.oracle.com/technetwork/java/codeconvtoc-136057.html
+ */
+
+/**
+ * This class is used to hold constants for the web service
+ */
 public class Constants {
     public static final String APPLICATION_PATH = "/bidder";
     public static final String PATH = "/services";
--- src/PlaceBid.java
+++ src/PlaceBid.java
@@ -3,32 +3,45 @@ import org.jeromq.ZMQ.*;
 import javax.ws.rs.*;
 import javax.ws.rs.core.Application;
 import javax.ws.rs.core.Response;
+import java.util.Date;
+
+/**
+ * @author Conor Hayes
+ */
 
 /*
-    @author Conor Hayes
     The official documentation was consulted for the third party library 0mq used in this class
     0mq pub -> https://github.com/zeromq/jeromq/blob/master/src/test/java/guide/pathopub.java
     Config -> http://www.mkyong.com/java/java-properties-file-examples/
-    URL for testing web service on localhost from browser:
-        http://localhost:8080/placebidservice/bidder/services/placebid/1/ch1987@live.ie
-    URL for AWS Web Service:
-        http://54.171.120.118:8080/placebidservice/bidder/services/placebid/1/ch1987@live.ie
-    Information researched for RESTful web service:
-        http://www.mkyong.com/webservices/jax-rs/jax-rs-pathparam-example/
+    Coding Standards -> http://www.oracle.com/technetwork/java/codeconvtoc-136057.html
+    URL for testing web service on localhost from browser -> http://localhost:8080/placebidservice/bidder/services/checkservice
+    URL for AWS Web Service -> http://54.171.120.118:8080/placebidservice/bidder/services/checkservice
+    RESTful web service -> http://www.mkyong.com/webservices/jax-rs/jax-rs-pathparam-example/
  */
 
+/**
+ * This class is a RESTful web service which handles user/bidder requests
+ */
 @ApplicationPath(Constants.APPLICATION_PATH)
 @Path(Constants.PATH)
 public class PlaceBid extends Application {
     private Context _context = ZMQ.context();
     private static Socket _publisher;
 
+    /**
+     * Consumed by bidders to place a bid
+     * @param id The ID of the auction
+     * @param email The email of the bidder
+     * @return The response code
+     */
     @GET
     @Path("placebid/{id}/{email}")
     public Response placeBid(@PathParam("id") String id, @PathParam("email") String email){
+        // Initialize the publisher, only done once
         if (_publisher == null) {
             _publisher = _context.socket(ZMQ.PUB);
             _publisher.bind(Constants.PUB_ADR);
+            // Allow time for the publisher to bind to the address
             try {
                 Thread.sleep(2000);
             } catch (InterruptedException e) {
@@ -36,12 +49,48 @@ public class PlaceBid extends Application {
                 return Response.status(Response.Status.INTERNAL_SERVER_ERROR).build();
             }
         }
+        // Build and publish the BidPlaced event
         String bidPlacedEvt = Constants.TOPIC + " <id>" + id + "</id> <params>" + email + "</params>";
         _publisher.send(bidPlacedEvt.getBytes());
         return Response.status(Response.Status.OK).build();
     }
 
+    /**
+     * Consumed by CheckServiceHealth service for monitoring purposes
+     * @return The response code
+     */
     @GET
     @Path("checkservice/")
     public Response checkService(){ return Response.status(200).build(); }
+
+    /**
+     * Consumed by users to register for a particular auction
+     * @param id The ID of the auction
+     * @param email The email of the user
+     * @return This web method will not be implemented
+     */
+    @POST
+    @Path("registerForAuction/{id}/{email}")
+    public Response registerForAuction(@PathParam("id") String id, @PathParam("email") String email){
+        throw new UnsupportedOperationException("Not implemented as part of this project");
+    }
+
+    /**
+     * Consumed to create an auction
+     * @param email The email of the seller
+     * @param itemName The name of the item
+     * @param description The description of the item
+     * @param startTime The start time of the auction
+     * @param estimatedValue The estimated value of the item
+     * @param startingBid The starting bid of the item
+     * @return
+     */
+    @POST
+    @Path("createAuction/{email}/{itemName}/{description}/{startTime}/{estimatedValue}/{startingBid}")
+    public Response createAuction(@PathParam("email") String email, @PathParam("itemName") String itemName,
+                                  @PathParam("description") String description, @PathParam("startTime") Date startTime,
+                                  @PathParam("estimatedValue") double estimatedValue,
+                                  @PathParam("startingBid") double startingBid){
+        throw new UnsupportedOperationException("Not implemented as part of this project");
+    }
 }
